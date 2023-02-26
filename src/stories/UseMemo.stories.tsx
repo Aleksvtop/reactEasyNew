c7918aee48b801cea88d4454ef8176558c545594
Aleksvtop <vazilino@gmail.com>
2023-02-26 23:12:39 +0300

    Create HelpsToReactMemo example for useMemo hook

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -40,3 +40,35 @@ export const DifficultCountingExample = () => {
         </div>
     </>
 }
+
+const UsersSecret = (props: {users: Array<string>}) => {
+    console.log('USERS')
+    return <div>{
+        props.users.map((u, i) => <div key={i}>{u}</div>)
+    }
+    </div>
+}
+const Users = React.memo(UsersSecret)
+
+export const HelpsToReactMemo = () => {
+    console.log('Helps to ReactMemo')
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem', 'Katya'])
+
+    const newArray = useMemo (() => {
+        const newArray = users.filter(u => u.toLowerCase().indexOf('a')>-1)
+        return newArray
+    },[users])
+
+    const addUser = () => {
+        const newUsers = [...users, 'Sveta ' + new Date().getTime()]
+        setUsers(newUsers)
+    }
+
+    return <>
+        <button onClick={() => setCounter(counter+1)}>+</button>
+        <button onClick={() => addUser()}>add user</button>
+        {counter}
+        <Users users={newArray}/>
+    </>
+}
